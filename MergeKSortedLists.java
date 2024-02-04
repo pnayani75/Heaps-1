319f88bd6b4e7b32d46fcf80d82aa2d122e92e4e
pnayani75 <35510785+pnayani75@users.noreply.github.com>
2024-02-03 16:48:11 -0800

    Create MergeKSortedLists.java

---
+++ MergeKSortedLists.java
@@ -0,0 +1,22 @@
+//tc: o(nklog(k)) sc: o(nk)
+class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)-> a.val-b.val);
+        for(ListNode head: lists){
+            if(head!=null){
+                pq.add(head);
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while(!pq.isEmpty()){
+            ListNode min = pq.poll();
+            curr.next = min;
+            curr = curr.next;
+            if(min.next != null){
+                pq.add(min.next);
+            }
+        }
+        return dummy.next;
+    }
+}
